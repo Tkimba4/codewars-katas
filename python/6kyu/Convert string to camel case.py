6922295376f11b3f37933657ff8c30995005f6fe
Terra Kimba <132136955+Tkimba4@users.noreply.github.com>
2026-05-19 16:48:43 +0100

    6kyu
    Implement function to convert dash/underscore delimited words into camel case.

---
+++ python/6kyu/Convert string to camel case.py
@@ -0,0 +1,19 @@
+"""
+Description:
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+"""
+
+def to_camel_case(text):
+    text = text.replace('_', " ").replace('-', " ")
+    tab = text.split(' ')
+    t = tab[0]
+    for s in tab[1:]:
+        t += s.capitalize()
+    return t
